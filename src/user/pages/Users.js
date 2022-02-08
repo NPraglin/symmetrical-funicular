1f7587dcf480abb74bb4d34245a7c1dd41035c6c
NPraglin <npraglin@gmail.com>
2022-02-08 16:22:53 -0600

    working userslist/useritems

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import UserList from '../components/UserList';
 
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [{
+    id: 'u1',
+    name: 'Nathan Praglin',
+    image: 'https://avatars.githubusercontent.com/u/2147533?v=4',
+    places: 5
+  }];
+
+  return <UserList items={USERS} />;
 };
 
 export default Users;
