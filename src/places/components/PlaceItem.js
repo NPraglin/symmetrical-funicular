f3f83826889cab825c595c677304fa52a22fbe1c
NPraglin <npraglin@gmail.com>
2022-02-10 16:53:22 -0600

    google maps api is broken..

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -3,6 +3,7 @@ import './PlaceItem.css';
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
 
 
 const PlaceItem = props => {
@@ -15,7 +16,8 @@ const PlaceItem = props => {
   return (
 
     <React.Fragment>
-        <Modal show={showMap} 
+        <Modal 
+          show={showMap} 
           onCancel={closeMapHandler} 
           header={props.address}
           contentClass='place-item__modal-content' 
@@ -23,7 +25,7 @@ const PlaceItem = props => {
           footer={<Button onClick={closeMapHandler} >Close</Button>} 
         >
           <div className='map-container'>
-            <h2>THE MAP!</h2>
+            <Map center={props.coordinates} zoom={16} />
           </div>
         </Modal>
       <li className='place-item'>
