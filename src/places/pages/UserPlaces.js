b0ee78934b192397838899c56a67ade33b65e865
NPraglin <npraglin@gmail.com>
2022-02-10 10:36:25 -0600

    working map slider, next step is google maps API

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -7,7 +7,7 @@ const DUMMY_PLACES =[
     id: 'p1',
     title: 'The Bean',
     description: 'Tourist attraction in Chicago by User 1',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/Cloud_Gate_%28The_Bean%29_from_east%27.jpg/340px-Cloud_Gate_%28The_Bean%29_from_east%27.jpg',
+    imageUrl: 'https://cdn.choosechicago.com/uploads/2019/07/first-time-bean-1.jpg',
     address: '201 E Randolph St, Chicago, IL 60602',
     location: {
       lat: 41.882702,
