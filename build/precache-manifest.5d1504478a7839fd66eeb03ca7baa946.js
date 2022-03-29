47d8621c67775a03e5228c0f1d723734362cf9fe
NPraglin <npraglin@gmail.com>
2022-03-29 15:38:34 -0500

    adjusted new place route

---
+++ build/precache-manifest.5d1504478a7839fd66eeb03ca7baa946.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a160d3281ab15145cb59e4fd808b155",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fa6f5e67ebb415ceaf7",
+    "url": "/static/css/0.1930bbfe.chunk.css"
+  },
+  {
+    "revision": "35985be50ebf2aa84265",
+    "url": "/static/css/4.e88e2a3f.chunk.css"
+  },
+  {
+    "revision": "756554c04ecb03ca6888",
+    "url": "/static/css/5.2adb76c7.chunk.css"
+  },
+  {
+    "revision": "b9e83c0315511ce2312d",
+    "url": "/static/css/6.299d9ff5.chunk.css"
+  },
+  {
+    "revision": "8a002aeca736e3bbab98",
+    "url": "/static/css/7.b2afc390.chunk.css"
+  },
+  {
+    "revision": "0c100bf5a86f8797a555",
+    "url": "/static/css/main.ee080649.chunk.css"
+  },
+  {
+    "revision": "3fa6f5e67ebb415ceaf7",
+    "url": "/static/js/0.bfd6981c.chunk.js"
+  },
+  {
+    "revision": "c9b5f3b8b19e4cf8b2c0",
+    "url": "/static/js/3.f38be934.chunk.js"
+  },
+  {
+    "revision": "35985be50ebf2aa84265",
+    "url": "/static/js/4.17b044e7.chunk.js"
+  },
+  {
+    "revision": "756554c04ecb03ca6888",
+    "url": "/static/js/5.fe9290d3.chunk.js"
+  },
+  {
+    "revision": "b9e83c0315511ce2312d",
+    "url": "/static/js/6.22138f14.chunk.js"
+  },
+  {
+    "revision": "8a002aeca736e3bbab98",
+    "url": "/static/js/7.87ec5e99.chunk.js"
+  },
+  {
+    "revision": "0c100bf5a86f8797a555",
+    "url": "/static/js/main.407ee4e6.chunk.js"
+  },
+  {
+    "revision": "b00d67e982b913461516",
+    "url": "/static/js/runtime-main.4a3734f0.js"
+  }
+]);
